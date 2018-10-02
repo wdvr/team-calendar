145f5210c0f4599f84190b0109cf85a05f37461a
Wouter Devriendt <wouterdevriendt@gmail.com>
2018-10-01 23:13:02 -0400

    api always empty

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const allowedExt = [
 ];
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 }
 
